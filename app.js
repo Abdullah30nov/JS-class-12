2a012510c4f86a0ce3e877ef71405a4510ad14c7
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-04-24 21:03:36 +0500

    DOM second class

---
+++ app.js
@@ -0,0 +1,46 @@
+// var heading = document.getElementById('heading');
+// console.log(heading);
+// console.log(heading.previousSibling);
+// console.log(heading.nextSibling);
+
+// var main = document.getElementById('main');
+// console.log(main.firstChild);
+// console.log(main.lastChild);
+// console.log(main.parentNode);
+// console.log(main.nodeName);
+// console.log(main.hasAttribute('id'));
+// console.log(main.hasAttribute('class'));
+// console.log(main.getAttribute('id'));
+// console.log(main.attributes[0].nodeType)
+
+// var link = document.getElementById('link');
+// link.innerText = "Google";
+// link.href = "https://www.google.com";
+// link.setAttribute("href","https://www.google.com");
+// console.log(link);
+
+var paraElement = document.createElement('h1');
+var paratext = document.createTextNode('tHIS IS h1 BIG hEADING');
+
+paraElement.appendChild(paratext);
+// var main = document.getElementById('main');
+console.log(paraElement)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CRUD operation = ( Create, Read, Update, Delete)
